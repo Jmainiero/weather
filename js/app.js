36511684c586774da701ccadb4d192e15c416a4a
John Mainiero <jcmainiero@gmail.com>
2020-06-02 21:00:28 -0400

    Adjusted UI / Begun javascript implementation

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const resultsSide = document.querySelector('.card__side-results');
 // console.log(defaultSide);
 // console.log(resultsSide);
 
+//ROTATE THE APP TO SHOW RESULTS
 // defaultSide.style.transform = 'rotateX(-180deg)';
 // resultsSide.style.transform = 'rotateX(0)';
 
@@ -14,8 +15,20 @@ document.querySelector(".form__weather").addEventListener("submit", function (ev
   const zip = document.querySelector('.form__weather--input');
   console.log(zip.value);
 
-  printData(fetchData(zip));
+  fetchData(zip.value);
+  // printData(fetchData(zip));
   defaultSide.style.transform = 'rotateX(-180deg)';
   resultsSide.style.transform = 'rotateX(0)';
   event.preventDefault()
 });
+
+
+const fetchData = (zip) => {
+  const apiKey = 'e07fdcbfcc218eb6cfbdeb7fec313fab';
+  const url = `api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}`;
+  console.log(url);
+
+  fetch('http://api.openweathermap.org/data/2.5/weather?zip=10306&appid=e07fdcbfcc218eb6cfbdeb7fec313fab')
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
